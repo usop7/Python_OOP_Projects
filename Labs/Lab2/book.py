f516d62f2a3a076a0571ebf721dba512d3ad4f16
Leeseul Kim <dewkim7@gmail.com>
2019-09-18 23:28:57 -0700

    added methods in book module

--- Labs/Lab2/book.py
+++ Labs/Lab2/book.py
@@ -4,3 +4,11 @@ class Book:
         self._call_number = call_number
         self._author = author
         self._num_copies = num_copies
+
+    def check_availability(self):
+        return self._num_copies >= 1
+
+    def __str__(self):
+        return f"title: {self._title}, call number: {self._call_number}, author: {self._author}, number of copies: {self._num_copies}"
+
+    
--- Labs/Lab2/library.py
+++ Labs/Lab2/library.py
@@ -1,3 +1,4 @@
 class Library:
     def __init__(self, book_list):
         self._book_list = book_list
+
