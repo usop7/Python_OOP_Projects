3baa2db487f14225265952024f0ae3a993801799
Leeseul Kim <dewkim7@gmail.com>
2019-09-18 23:20:19 -0700

    Initial Commit

---
+++ Labs/Lab2/book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, call_number, author, num_copies):
+        self._title = title
+        self._call_number = call_number
+        self._author = author
+        self._num_copies = num_copies
---
+++ Labs/Lab2/library.py
@@ -0,0 +1,3 @@
+class Library:
+    def __init__(self, book_list):
+        self._book_list = book_list
