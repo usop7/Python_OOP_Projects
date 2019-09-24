62e1b7c97664fe2622b2ae87fd371a3b2558380a
Leeseul Kim <dewkim7@gmail.com>
2019-09-24 00:22:32 -0700

    Updated Console

---
+++ Assignments/Assignment1/console.py
@@ -0,0 +1,36 @@
+from tamagotchiController import TamagotchiCreator
+from tamagotchiController import TamagotchiManager
+
+
+class Console:
+    """This class embodies the basic functions and attributes of a controller"""
+
+    def __init__(self):
+        self._tamagotchi = None
+
+    def start_game(self):
+        """
+        If there is no tamagotchi, ask user if they want to hatch one, and call Tamagotchi Creator.
+        If there is a tamagotchi, call TamagotchiManager and give menu.
+        :return:
+        """
+        while True:
+            if self._tamagotchi is None or self._tamagotchi.is_alive() is False:
+                answer = input(">> No tamagotchi exists. Do you want to hatch one? (Y/N): ")
+                if answer.lower() == "y":
+                    self._tamagotchi = TamagotchiCreator.hatch()
+            else:
+                manager = TamagotchiManager(self._tamagotchi)
+                manager.give_menu()
+
+
+def main():
+    """
+    Creates a controller and simulates the asteroids' movements.
+    """
+    console = Console()
+    console.start_game()
+
+
+if __name__ == "__main__":
+    main()
