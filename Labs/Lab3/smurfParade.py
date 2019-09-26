e9109dfb3a7d31a68127fbc72b4fcb6ae1e413da
Leeseul Kim <dewkim7@gmail.com>
2019-09-25 19:06:51 -0700

    Implemented it as a linked list

--- Labs/Lab3/smurfParade.py
+++ Labs/Lab3/smurfParade.py
@@ -1,96 +1,109 @@
 class Smurf:
     """This class ."""
 
-    def __init__(self, name, characteristic):
+    def __init__(self, name, next_smurf=None):
         self.name = name
-        self.characteristic = characteristic
+        self.next_smurf = next_smurf
 
     def __str__(self):
-        return f"Name: {self.name}\nCharacteristic: {self.characteristic}"
+        return f"{self.name}"
 
 
 class SmurfParade:
     """This  class ."""
 
-    def __init__(self, village, smurf_list):
-        self.village = village
-        self.smurf_list = smurf_list
+    def __init__(self, head=None):
+        self.head = head
+
+    def add(self, name):
+        new_smurf = Smurf(name)
+        curr_smurf = self.head
+        if curr_smurf is None:
+            self.head = new_smurf
+        else:
+            while curr_smurf.next_smurf is not None:
+                curr_smurf = curr_smurf.next_smurf
+            curr_smurf.next_smurf = new_smurf
 
     def __str__(self):
         smurf_list = ""
-        for smurf in self.smurf_list:
-            smurf_list += smurf.name + " "
-        return f"{self.village} - {smurf_list}"
+        curr_smurf = self.head
+        while curr_smurf is not None:
+            smurf_list += curr_smurf.name + " "
+            curr_smurf = curr_smurf.next_smurf
+        return smurf_list
 
     def __len__(self):
-        """Returns the number of smurf_list in the smurf list.
-        :returns: an int"""
-        return len(self.smurf_list)
+        count = 0
+        curr_smurf = self.head
+        while curr_smurf is not None:
+            count += 1
+            curr_smurf = curr_smurf.next_smurf
+        return count
 
     def __contains__(self, item):
-        """Returns True if a given smurf name belongs to the smurf list.
-        :param item: a String
-        :returns: a boolean"""
-        for smurf in self.smurf_list:
-            if smurf.name == item:
+        curr_smurf = self.head
+        while curr_smurf is not None:
+            if curr_smurf.name == item:
                 return True
+            curr_smurf = curr_smurf.next_smurf
         return False
 
-    def __iter__(self):
-        """Overriding this method so that it can iterate its smurf_list list."""
-        return iter(self.smurf_list)
+    #def __iter__(self):
+     #   return iter(self.smurf_list)
 
     def __getitem__(self, key):
-        """
-        Returns smurf object whose index equals the key in smurf list.
-        :param key: an int
-        :return: Smurf
-        """
         index = 0
-        for smurf in self.smurf_list:
+        curr_smurf = self.head
+        while curr_smurf is not None:
             if index == key:
-                return smurf
+                return curr_smurf
             index += 1
+            curr_smurf = curr_smurf.next_smurf
         return None
 
     def count(self, item):
         """
-        Returns the number of smurf_list whose name is same as an item.
+        Returns the number of smurf whose name is same as an item.
         :param item: a String
         :return: an int
         """
         count = 0
-        for smurf in self.smurf_list:
-            if smurf.name == item:
+        curr_smurf = self.head
+        while curr_smurf is not None:
+            if curr_smurf.name == item:
                 count += 1
+            curr_smurf = curr_smurf.next_smurf
         return count
 
     def index(self, item):
         """Returns the first index of Smurf in the smurf list whose name equals to an item."""
         index = 0
-        for smurf in self.smurf_list:
-            index += 1
-            if smurf.name == item:
+        curr_smurf = self.head
+        while curr_smurf is not None:
+            if curr_smurf.name == item:
                 return index
+            index += 1
+            curr_smurf = curr_smurf.next_smurf
         return None
 
-    def __reversed__(self):
-        """Returns a reversed list of smurf list."""
-        return SmurfParade(self.village, list(reversed(self.smurf_list)))
 
 
 def main():
-    parade = SmurfParade("vellage1",
-                         [Smurf("leeseul", 10),
-                          Smurf("michelle", 20),
-                          Smurf("doris", 15)])
+    parade = SmurfParade()
+    parade.add("leeseul")
+    parade.add("christy")
+    parade.add("robert")
+    parade.add("justin")
+    parade.add("eric")
 
     print(f"\n1. parade: {parade}")
     print(f"\n2. len(parade): {len(parade)}")
     print(f"\n3. parade[0]:\n{parade[0]}")
     print(f"\n4. 'leeseul' in parade: {'leeseul' in parade}")
     print(f"\n5. 'leeseul' not in parade: {'leeseul' not in parade}")
-    print(f"\nreversed(parade): {reversed(parade)}")
+    #print(f"\nreversed(parade): {reversed(parade)}")
+
 
 
 if __name__ == '__main__':
