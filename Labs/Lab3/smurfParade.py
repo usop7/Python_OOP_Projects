8c5cfebd10c17bb1201de199738d68646e09dedc
Leeseul Kim <dewkim7@gmail.com>
2019-09-25 17:43:51 -0700

    Added count, index, reversed methods

--- Labs/Lab3/smurfParade.py
+++ Labs/Lab3/smurfParade.py
@@ -12,34 +12,71 @@ class Smurf:
 class SmurfParade:
     """This  class ."""
 
-    def __init__(self, village, smurfs):
+    def __init__(self, village, smurf_list):
         self.village = village
-        self.smurfs = smurfs
+        self.smurf_list = smurf_list
 
     def __str__(self):
         smurf_list = ""
-        for smurf in self.smurfs:
+        for smurf in self.smurf_list:
             smurf_list += smurf.name + " "
-        return f"{self.village}: {smurf_list}"
+        return f"{self.village} - {smurf_list}"
 
     def __len__(self):
-        return len(self.smurfs)
+        """Returns the number of smurf_list in the smurf list.
+        :returns: an int"""
+        return len(self.smurf_list)
 
-    def __contains__(self, smurf_name):
-        found = False
-        for smurf in self.smurfs:
-            if smurf.name == smurf_name:
-                found = True
-        return found
+    def __contains__(self, item):
+        """Returns True if a given smurf name belongs to the smurf list.
+        :param item: a String
+        :returns: a boolean"""
+        for smurf in self.smurf_list:
+            if smurf.name == item:
+                return True
+        return False
 
     def __iter__(self):
-        return iter(self.smurfs)
+        """Overriding this method so that it can iterate its smurf_list list."""
+        return iter(self.smurf_list)
 
     def __getitem__(self, key):
-        for smurf in self.smurfs:
-            if smurf.name == key:
+        """
+        Returns smurf object whose index equals the key in smurf list.
+        :param key: an int
+        :return: Smurf
+        """
+        index = 0
+        for smurf in self.smurf_list:
+            if index == key:
                 return smurf
+            index += 1
         return None
+
+    def count(self, item):
+        """
+        Returns the number of smurf_list whose name is same as an item.
+        :param item: a String
+        :return: an int
+        """
+        count = 0
+        for smurf in self.smurf_list:
+            if smurf.name == item:
+                count += 1
+        return count
+
+    def index(self, item):
+        """Returns the first index of Smurf in the smurf list whose name equals to an item."""
+        index = 0
+        for smurf in self.smurf_list:
+            index += 1
+            if smurf.name == item:
+                return index
+        return None
+
+    def __reversed__(self):
+        """Returns a reversed list of smurf list."""
+        return reversed(self.smurf_list)
 
 
 def main():
@@ -48,11 +85,13 @@ def main():
                           Smurf("michelle", 20),
                           Smurf("doris", 15)])
 
-    print(f"\nparade:\n{parade}")
+    print(f"\nparade: {parade}")
 
     print(f"\nlen(parade): {len(parade)}")
 
-    print(f"\nparade['leeseul']:\n {parade['leeseul']}")
+    print(f"\nparade[0]:\n{parade[0]}")
+
+    print(f"\nreversed(parade): {list(reversed(parade))}")
 
 
 if __name__ == '__main__':
