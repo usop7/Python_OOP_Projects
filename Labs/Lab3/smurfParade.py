ac93e6e782bc492151feb2d8fda7e6d2a748d3be
Leeseul Kim <dewkim7@gmail.com>
2019-09-25 21:00:05 -0700

    Updated main method for testing purposes

--- Labs/Lab3/smurfParade.py
+++ Labs/Lab3/smurfParade.py
@@ -1,21 +1,28 @@
 class Smurf:
-    """This class ."""
+    """This class represents a smurf as a Node with a name value."""
 
     def __init__(self, name, next_smurf=None):
         self.name = name
         self.next_smurf = next_smurf
 
     def __str__(self):
+        """
+        Returns a name.
+        :returns: a String
+        """
         return f"{self.name}"
 
 
 class SmurfParade:
-    """This  class ."""
+    """This  class represents a Linked List of smurfs(Nodes)."""
 
     def __init__(self, head=None):
         self.head = head
 
     def add(self, name):
+        """
+        Creates a new smurf with a given name and appends it to the end of the list.
+        """
         new_smurf = Smurf(name)
         curr_smurf = self.head
         if curr_smurf is None:
@@ -26,6 +33,10 @@ class SmurfParade:
             curr_smurf.next_smurf = new_smurf
 
     def __str__(self):
+        """
+        Returns a list of smurfs' names.
+        :returns: a List
+        """
         smurf_list = ""
         curr_smurf = self.head
         while curr_smurf is not None:
@@ -34,6 +45,10 @@ class SmurfParade:
         return smurf_list
 
     def __len__(self):
+        """
+        Returns the number of Smurfs in the list.
+        :return: an int
+        """
         count = 0
         curr_smurf = self.head
         while curr_smurf is not None:
@@ -42,6 +57,11 @@ class SmurfParade:
         return count
 
     def __contains__(self, item):
+        """
+        Returns True if the list has a smurf whose name equals to the parameter, item.
+        :param item: a String
+        :return: boolean
+        """
         curr_smurf = self.head
         while curr_smurf is not None:
             if curr_smurf.name == item:
@@ -50,6 +70,9 @@ class SmurfParade:
         return False
 
     def __iter__(self):
+        """
+        Overrides iter method so that it can iterate smurfs in the list.
+        :returns: iterable object"""
         smurf_list = []
         curr_smurf = self.head
         while curr_smurf is not None:
@@ -58,6 +81,11 @@ class SmurfParade:
         return iter(smurf_list)
 
     def __getitem__(self, key):
+        """
+        Returns a Smurf whose index equals to the parameter, key.
+        :param key: an int
+        :return: a Smurf
+        """
         index = 0
         curr_smurf = self.head
         while curr_smurf is not None:
@@ -69,7 +97,7 @@ class SmurfParade:
 
     def count(self, item):
         """
-        Returns the number of smurf whose name is same as an item.
+        Returns the number of smurfs whose name is same as the parameter, item.
         :param item: a String
         :return: an int
         """
@@ -82,7 +110,11 @@ class SmurfParade:
         return count
 
     def index(self, item):
-        """Returns the first index of Smurf in the smurf list whose name equals to an item."""
+        """
+        Returns the first index of Smurf in the smurf list whose name equals to an item.
+        :param item: a String
+        :return: an int
+        """
         index = 0
         curr_smurf = self.head
         while curr_smurf is not None:
@@ -92,28 +124,43 @@ class SmurfParade:
             curr_smurf = curr_smurf.next_smurf
         return None
 
+    def __reversed__(self):
+        """
+        Creates a reversed linked list and return it.
+        :return: a SmurfParade
+        """
+        prev_smurf = None
+        curr_smurf = self.head
+        while curr_smurf is not None:
+            next_temp = curr_smurf.next_smurf
+            curr_smurf.next_smurf = prev_smurf
+            prev_smurf = curr_smurf
+            curr_smurf = next_temp
+        return SmurfParade(prev_smurf)
 
 
 def main():
-    parade = SmurfParade()
-    parade.add("leeseul")
-    parade.add("christy")
-    parade.add("robert")
-    parade.add("justin")
-    parade.add("eric")
+    """Creates a SmurfParade object and tests its methods."""
 
-    print(f"\n1. parade: {parade}")
-    print(f"\n2. len(parade): {len(parade)}")
-    print(f"\n3. parade[0]:\n{parade[0]}")
-    print(f"\n4. 'leeseul' in parade: {'leeseul' in parade}")
-    print(f"\n5. 'leeseul' not in parade: {'leeseul' not in parade}")
-    print("\n------------Iteration Test-----------")
+    parade = SmurfParade()
+    parade.add("Leeseul")
+    parade.add("Christy")
+    parade.add("Robert")
+    parade.add("Justin")
+    parade.add("Eric")
+
+    print(f"1. parade: {parade}")
+    print(f"2. len(parade): {len(parade)}")
+    print(f"3. parade[1]:{parade[1]}")
+    print(f"4. parade[6]:{parade[6]}")
+    print(f"5. 'Leeseul' in parade: {'Leeseul' in parade}")
+    print(f"6. 'Leeseul' not in parade: {'Leeseul' not in parade}")
+    print("------------Testing Iteration-----------")
     for smurf in parade:
         print(smurf)
     print("------------End of Iteration-----------")
 
-    #print(f"\nreversed(parade): {reversed(parade)}")
-
+    print(f"\nreversed(parade): {reversed(parade)}")
 
 
 if __name__ == '__main__':
