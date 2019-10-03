7377b95294ed96135b81f0276f08c626599c6093
Leeseul Kim <dewkim7@gmail.com>
2019-10-03 15:10:39 -0700

    Code Refactoring

--- Labs/Lab4/dictionary.py
+++ Labs/Lab4/dictionary.py
@@ -22,73 +22,89 @@ class Dictionary:
     def __init__(self):
         self.dictionary = {}
 
+    @classmethod
+    def print_query_result(cls, word, definitions):
+        """
+        Prints out the word and the definitions.
+        :param word: String
+        :param definitions: List
+        """
+        count = 0
+        print(f"\n---------------------------------------------\n"
+              f"Query Results for [{word}]:")
+        for definition in definitions:
+            count += 1
+            definition = definition.replace('\\n', ' ')
+            print(f"{count}. {definition}")
+        print("----------------------------------------------")
+
+    @classmethod
+    def save_query_result(cls, path, word, definitions):
+        """
+        Appends the word and the definitions into a text file.
+        :param path: String
+        :param word: String
+        :param definitions: List
+        """
+        count = 0
+        FileHandler.write_lines(path, word)
+        for definition in definitions:
+            count += 1
+            definition = definition.replace('\\n', ' ')
+            FileHandler.write_lines(f"{count}. {definition}")
+
     def load_dictionary(self, filepath):
         """
         It loads data from the given filepath into a dictionary.
-        If file as succesfully loaded, return True, otherwise return false.
+        If the file is uccesfully loaded, return True, otherwise return false.
         :param filepath: String
         :return: boolean
         """
-        self.dictionary = json.loads(FileHandler.load_data(filepath))
-
-    def write_query_result(self, filepath, word, definitions):
-        """
-        It appends the query result into a text file.
-        :param filepath: String
-        :param result: Dictionary
-        """
-        FileHandler.write_lines(filepath, word)
-        for definition in definitions:
-            FileHandler.write_lines(filepath, definition)
-
+        loaded = False
+        try:
+            self.dictionary = json.loads(FileHandler.load_data(filepath))
+        except FileNotFoundError as e:
+            print(f"{e}")
+        except InvalidFileTypeError as e:
+            print(f"{e}")
+        except json.decoder.JSONDecodeError as e:
+            print(f"Input file format is not compatible for dictionary: {e}")
+        except Exception as e:
+            print(f"Unknown Exception caught: {e}")
+        else:
+            loaded = True
+        finally:
+            return loaded
 
     def query_definition(self, word):
         """
         It looks for a given word and returns the definitions.
+        if there is no exact match, raise WordNotFoundException
         :param word: String
         :return: List
         """
-        definitions = self.dictionary.get(word)
-        return definitions
+        if self.dictionary.get(word) is None:
+            raise WordNotFoundException(word, self.dictionary.keys())
+        return self.dictionary.get(word)
 
     def run_program(self):
         """
         Keep prompting users with the option to query definitions until
         the user has entered 'exitprogram'.
-        1) If there is an exact match, show the definition(s) and save the
-           query results into a file.
-        2) If there is no exact match, but close matches exist, show the
-           recommended words.
-        3) If there is no exact match nor close matches, print no word found.
         """
         EXIT_COMMAND = "exitprogram"
-        FILE_TO_SAVE = "result.txt"
         word = ""
         while word != EXIT_COMMAND:
-            count = 0  # number of definitions.
             word = input("\nPlease type a word to search (Type 'exitprogram'"
-                         " if you want to stop the program): ").lower()
-            result = self.query_definition(word)
-            close_words = get_close_matches(word, self.dictionary.keys())
-
-            if word == EXIT_COMMAND:
-                print("Bye!")
-                return
-            elif result is None and len(close_words) == 0:
-                print("No word found.\n")
-            elif result is not None:
-                print(f"\n---------------------------------------------\n"
-                      f"Query Results for [{word}]:")
-                FileHandler.write_lines(FILE_TO_SAVE, f"\n{word}")
-                for definition in result:
-                    count += 1
-                    definition = definition.replace('\\n', ' ')
-                    print(f"{count}. {definition}")
-                    FileHandler.write_lines(FILE_TO_SAVE,
-                                            f"{count}. {definition}")
-                print("----------------------------------------------")
+                         " if you want to stop the program): ")
+            try:
+                result = self.query_definition(word)
+            except WordNotFoundException as e:
+                print(f"{e}")
             else:
-                print(f"Were you looking for a word among {close_words} ?")
+                Dictionary.print_query_result(word, result)
+                Dictionary.print_query_result(word, result)
+
 
 
 def main():
@@ -96,26 +112,9 @@ def main():
 
     my_dictionary = Dictionary()
 
-    # Loads input file, and if loading was successful, runs the program.
-    loaded = False
-    INPUT_FILE = "data.json"
-    try:
-        my_dictionary.load_dictionary(INPUT_FILE)
-    except FileNotFoundError as e:
-        print(f"{e}")
-    except InvalidFileTypeError as e:
-        print(f"{e}")
-    except json.decoder.JSONDecodeError as e:
-        print(f"Input file format is not compatible for dictionary: {e}")
-    except Exception as e:
-        print(f"Unknown Exception caught: {e}")
-    else:
-        loaded = True
-    finally:
-        if loaded:
-            my_dictionary.run_program()
-        else:
-            print("Failed to load the file.")
+    # Loads the dictionary file, if succesful, runs the program.
+    if my_dictionary.load_dictionary("data.json"):
+        my_dictionary.run_program()
 
 
 if __name__ == "__main__":
