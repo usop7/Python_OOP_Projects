580a5e88b2f76a18ecd0d8f089513c5c1c43a97a
Leeseul Kim <dewkim7@gmail.com>
2019-10-29 09:46:07 -0700

    - Moved and updated menu method to input handler module. - Added Topping, ToppingMenu classes in ingredients module.

--- Assignments/Assignment3/ingredients.py
+++ Assignments/Assignment3/ingredients.py
@@ -1,5 +1,5 @@
 from input_handler import InputHandler
-from input_handler import CommandNotFoundException
+
 
 class Cheese:
     """This class represents a cheese."""
@@ -14,47 +14,76 @@ class Cheese:
         self.price = price
 
     def __str__(self):
-        return f"{self.name} ({self.price} calorie)"
+        return f"{self.name} (${self.price})"
 
 
 class CheeseMenu:
-    """This class embodies a list of food and food related methods."""
+    """This class embodies a list of cheese and a methods that give a
+    cheese menu option to users."""
 
     def __init__(self):
-        """
-        Initialize a food  controller.
-        """
+        """Initialize a cheese menu with a list of cheese."""
         self._cheese_list = [
             Cheese("Parmigiano Reggiano", 4.99),
             Cheese("Fresh Mozzarella", 3.99),
             Cheese("Vegan Cheese", 5.99)
         ]
 
-    def give_cheese_option(self):
+    def select_cheese(self):
         """
         Prompt user with cheese list, and return the selected cheese.
         :return: Cheese object
         """
-        question = "\nSelect your cheese!\n" \
-                   "----------------------------------\n"
+        question = f"\nSelect your cheese!\n{'-'*40}\n"
         i = 1
         for cheese in self._cheese_list:
             question += f"{i}: {cheese}\n"
             i += 1
-        question += "4: Skip\n" \
-                    "----------------------------------\n" \
-                    "Please select: "
+        question += f"{i}: Skip\n{'-'*40}\nPlease select: "
+        return InputHandler.prompt_menu(question, self._cheese_list)
 
-        while True:
-            answer = input(question)
-            if answer == str(len(self._cheese_list) + 1):
-                return None
-            else:
-                try:
-                    InputHandler.validate(len(self._cheese_list)+1, answer)
-                except ValueError:
-                    print("\n[Error] Please type an integer!")
-                except CommandNotFoundException as e:
-                    print(f"{e}")
-                else:
-                    return self._cheese_list[int(answer) - 1]
+
+class Topping:
+    """This class represents a topping."""
+
+    def __init__(self, name, price):
+        """
+        Initialize a topping item.
+        :param name: a String
+        :param price: a float
+        """
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"{self.name} (${self.price})"
+
+
+class ToppingMenu:
+    """This class embodies a list of topping and a methods that give a
+    topping menu option to users."""
+
+    def __init__(self):
+        """Initialize a topping menu with a list of toppings."""
+        self._topping_list = [
+            Topping("Peppers", 1.5),
+            Topping("Pineapple", 2),
+            Topping("Mushrooms", 1.5),
+            Topping("Fresh Basil", 2),
+            Topping("Spinach", 1),
+            Topping("Pepperoni", 3),
+            Topping("Beyond Meat", 4)
+        ]
+
+    def select_topping(self):
+        """
+        Prompt user with a topping list, and return the selected topping.
+        :return: None or Topping object
+        """
+        question = f"\nSelect your topping!\n{'-'*40}\n"
+        i = 1
+        for topping in self._topping_list:
+            question += f"{i}: {topping}\n"
+            i += 1
+        question += f"{i}: Skip\n{'-'*40}\nPlease select: "
+        return InputHandler.prompt_menu(question, self._topping_list)
--- Assignments/Assignment3/input_handler.py
+++ Assignments/Assignment3/input_handler.py
@@ -6,8 +6,8 @@ class CommandNotFoundException(Exception):
     This exception will be raised when there is no matching command found.
     """
 
-    def __init__(self, num_commands):
-        super().__init__(f"\n[Error] Please select between 1 to {num_commands}")
+    def __init__(self, num):
+        super().__init__(f"\n[Error] Please select between 1 to {num}")
 
 
 class InputHandler:
@@ -26,3 +26,27 @@ class InputHandler:
             raise ValueError
         if int(user_input) < 1 or int(user_input) > num_commands:
             raise CommandNotFoundException(num_commands)
+
+    @staticmethod
+    def prompt_menu(question, option_list):
+        """
+        Prompt user with a list of options, and returns the selected option.
+        The 'skip' option will be added to the end of the given list.
+        :param question: String
+        :param option_list: list
+        :return: None or an Item in the list
+        """
+        while True:
+            answer = input(question)
+            # When choose to 'skip'(last option)
+            if answer == str(len(option_list) + 1):
+                return None
+            else:
+                try:
+                    InputHandler.validate(len(option_list) + 1, answer)
+                except ValueError:
+                    print("\n[Error] Please type an integer!")
+                except CommandNotFoundException as e:
+                    print(f"{e}")
+                else:
+                    return option_list[int(answer) - 1]
