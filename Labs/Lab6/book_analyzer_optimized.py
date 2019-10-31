fa1541b7342f06ad27c68dbb2fdcfe4c2bf39717
Leeseul Kim <dewkim7@gmail.com>
2019-10-30 21:35:29 -0700

    - Deleted is unique method - change list to dictionary for text file

--- Labs/Lab6/book_analyzer_optimized.py
+++ Labs/Lab6/book_analyzer_optimized.py
@@ -3,6 +3,7 @@ This module is responsible for holding a badly written (but not so bad
 that you won't find this in the workplace) BookAnalyzer class that needs
 to be profiled and optimized.
 """
+import string
 
 
 class BookAnalyzer:
@@ -16,7 +17,7 @@ class BookAnalyzer:
     COMMON_PUNCTUATION = [",", "*", ";", ".", ":", "(", "[", "]", ")"]
 
     def __init__(self):
-        self.text = None
+        self.text = {}
 
     def read_data(self, src="House of Usher.txt"):
         """
@@ -26,53 +27,30 @@ class BookAnalyzer:
         :param src: the name of the file, a string
         """
         # read lines
+        word_counter = {}
         with open(src, mode='r', encoding='utf-8') as book_file:
-            self.text = [word.lower() for line in book_file
-                         for word in line.split()]
-
-        # remove common punctuation from words
-        temp_text = []
-        for word in self.text:
-            temp_word = word
-            for punctuation in self.COMMON_PUNCTUATION:
-                temp_word = temp_word.replace(punctuation, '')
-            temp_text.append(temp_word)
-        self.text = temp_text
-
-
-    @staticmethod
-    def is_unique(word, word_list):
-        """
-        Checks to see if the given word appears in the provided sequence.
-        This check is case in-sensitive.
-        :param word: a string
-        :param word_list: a sequence of words
-        :return: True if not found, false otherwise
-        """
-        return word_list.count(word.lower()) == 0
+            for line in book_file:
+                for word in line.split():
+                    word = word.translate(str.maketrans('', '', string.punctuation)).lower()
+                    if word_counter.get(word) is None:
+                        word_counter[word] = 1
+                    else:
+                        word_counter[word] = word_counter.get(word) + 1
+        self.text = word_counter
 
     def find_unique_words(self):
         """
         Filters out all the words that only appear once in the text.
         :return: a list of all the unique words.
         """
-        temp_text = self.text
-        unique_words = []
-        non_unique_words = []
-        while temp_text:
-            word = temp_text.pop().lower()
-            if word in non_unique_words:
-                continue
-            if self.is_unique(word, temp_text):
-                unique_words.append(word)
-            else:
-                non_unique_words.append(word)
+        unique_words = [word for (word, count) in self.text.items() if count == 1]
         return unique_words
 
 
 def main():
     book_analyzer = BookAnalyzer()
     book_analyzer.read_data()
+    print(len(book_analyzer.text))
     unique_words = book_analyzer.find_unique_words()
     print("-"*50)
     print(f"List of unique words (Count: {len(unique_words)})")
