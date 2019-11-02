68675212a4056e1ba57418d40bc8eb6e4b270b98
Leeseul Kim <dewkim7@gmail.com>
2019-11-02 16:58:23 -0700

    updated ingredients module

--- Assignments/Assignment3/ingredients.py
+++ Assignments/Assignment3/ingredients.py
@@ -1,4 +1,14 @@
+from enum import Enum
 from input_handler import InputHandler
+
+
+class SignatureCrust:
+    def __init__(self):
+        self.name = "Signature Crust"
+        self.price = 4.99
+
+    def __str__(self):
+        return f"{self.name}: $ {self.price}"
 
 
 class Cheese:
@@ -7,40 +17,39 @@ class Cheese:
     def __init__(self, name, price):
         """
         Initialize a cheese item.
-        :param name: a String
+        :param type_: a String
         :param price: a float
         """
         self.name = name
         self.price = price
 
     def __str__(self):
-        return f"{self.name} (${self.price})"
+        return f"{self.name}: $ {self.price}"
 
 
 class CheeseMenu:
     """This class embodies a list of cheese and a methods that give a
     cheese menu option to users."""
 
-    def __init__(self):
-        """Initialize a cheese menu with a list of cheese."""
-        self._cheese_list = [
-            Cheese("Parmigiano Reggiano", 4.99),
-            Cheese("Fresh Mozzarella", 3.99),
-            Cheese("Vegan Cheese", 5.99)
-        ]
+    cheese_list = [
+        Cheese("Parmigiano Reggiano", 4.99),
+        Cheese("Fresh Mozzarella", 3.99),
+        Cheese("Vegan Cheese", 5.99)
+    ]
 
-    def select_cheese(self):
+    @staticmethod
+    def select_cheese():
         """
         Prompt user with cheese list, and return the selected cheese.
         :return: Cheese object
         """
-        question = f"\nSelect your cheese!\n{'-'*40}\n"
+        question = f"\nStep 1: Select your cheese!\n{'-'*40}\n"
         i = 1
-        for cheese in self._cheese_list:
-            question += f"{i}: {cheese}\n"
+        for cheese in CheeseMenu.cheese_list:
+            question += f"{i}. {cheese}\n"
             i += 1
-        question += f"{i}: Skip\n{'-'*40}\nPlease select: "
-        return InputHandler.prompt_menu(question, self._cheese_list)
+        question += f"{i}. Next\n{'-'*40}\nPlease select: "
+        return InputHandler.prompt_menu(question, CheeseMenu.cheese_list)
 
 
 class Topping:
@@ -56,34 +65,33 @@ class Topping:
         self.price = price
 
     def __str__(self):
-        return f"{self.name} (${self.price})"
+        return f"{self.name}: $ {self.price}"
 
 
 class ToppingMenu:
     """This class embodies a list of topping and a methods that give a
     topping menu option to users."""
 
-    def __init__(self):
-        """Initialize a topping menu with a list of toppings."""
-        self._topping_list = [
-            Topping("Peppers", 1.5),
-            Topping("Pineapple", 2),
-            Topping("Mushrooms", 1.5),
-            Topping("Fresh Basil", 2),
-            Topping("Spinach", 1),
-            Topping("Pepperoni", 3),
-            Topping("Beyond Meat", 4)
-        ]
+    topping_list = [
+        Topping("Peppers", 1.5),
+        Topping("Pineapple", 2),
+        Topping("Mushrooms", 1.5),
+        Topping("Fresh Basil", 2),
+        Topping("Spinach", 1),
+        Topping("Pepperoni", 3),
+        Topping("Beyond Meat", 4)
+    ]
 
-    def select_topping(self):
+    @staticmethod
+    def select_topping():
         """
         Prompt user with a topping list, and return the selected topping.
         :return: None or Topping object
         """
         question = f"\nSelect your topping!\n{'-'*40}\n"
         i = 1
-        for topping in self._topping_list:
-            question += f"{i}: {topping}\n"
+        for topping in ToppingMenu.topping_list:
+            question += f"{i}. {topping}\n"
             i += 1
-        question += f"{i}: Skip\n{'-'*40}\nPlease select: "
-        return InputHandler.prompt_menu(question, self._topping_list)
+        question += f"{i}. Check Out\n{'-'*40}\nPlease select: "
+        return InputHandler.prompt_menu(question, ToppingMenu.topping_list)
