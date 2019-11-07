aa4937411e2e0918eb5af7ecd72daa79674cfd1b
Leeseul Kim <dewkim7@gmail.com>
2019-11-06 16:50:57 -0800

    Redesigned LibraryItemGenerator using Factory Pattern

--- Labs/Lab7/item.py
+++ Labs/Lab7/item.py
@@ -144,41 +144,50 @@ class Journal(Item):
         return description
 
 
-class LibraryItemGenerator:
-    """This class embodies a function to generate a library item."""
+class ItemFactory(abc.ABC):
+    """
+    The ItemFactory class is the base class that the rest of the system
+    depends on. It defines a factory interface that creates an Item.
+    """
 
-    @staticmethod
-    def create_item():
-        """
-        Create an item based on user inputs and returns the item object.
-        It will prompt the user with various questions based on the item type.
-        :return: an item object
-        """
-        item = None
+    @abc.abstractmethod
+    def create_item(self) -> Item:
+        pass
 
-        # Repeat until the user enters valid types of an item.
-        item_type = None
-        while item_type not in ["book", "dvd", "journal"]:
-            item_type = input("Item Type (Book, DVD, Journal): ").lower()
 
-        # Common questions about an item.
+class BookFactory(ItemFactory):
+    """The BookFactory is responsible for creating a Book Item."""
+
+    @abc.abstractmethod
+    def create_item(self) -> Item:
         title = input("Title: ")
         call_number = input("Call Number: ")
         num_copies = int(input("Number of Copies: "))
+        author = input("Author: ")
+        return Book(title, call_number, num_copies, author)
 
-        # Ask additional questions based on the type of an item.
-        if item_type == "book":
-            author = input("Author: ")
-            item = Book(title, call_number, num_copies, author)
-        elif item_type == "dvd":
-            release_date = input("Release Date: ")
-            region_code = input("Region Code: ")
-            item = DVD(title, call_number, num_copies, release_date, region_code)
-        elif item_type == "journal":
-            issue_number = input("Issue Number: ")
-            publisher = input("Publisher: ")
-            item = Journal(title, call_number, num_copies, issue_number, publisher)
-        else:
-            print("Wrong Type")
 
-        return item
+class DvdFactory(ItemFactory):
+    """The DvdFactory is responsible for creating a DVD Item."""
+
+    @abc.abstractmethod
+    def create_item(self) -> Item:
+        title = input("Title: ")
+        call_number = input("Call Number: ")
+        num_copies = int(input("Number of Copies: "))
+        release_date = input("Release Date: ")
+        region_code = input("Region Code: ")
+        return DVD(title, call_number, num_copies, release_date, region_code)
+
+
+class JournalFactory(ItemFactory):
+    """The JournalFactory is responsible for creating a Journal Item."""
+
+    @abc.abstractmethod
+    def create_item(self) -> Item:
+        title = input("Title: ")
+        call_number = input("Call Number: ")
+        num_copies = int(input("Number of Copies: "))
+        issue_number = input("Issue Number: ")
+        publisher = input("Publisher: ")
+        return Journal(title, call_number, num_copies, issue_number, publisher)
