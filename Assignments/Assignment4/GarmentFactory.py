06f020b3595de13602fc56cd135c9b9995447fdb
Leeseul Kim <dewkim7@gmail.com>
2019-11-17 19:25:55 -0800

    Created BrandFactory class and its child factories

--- Assignments/Assignment4/GarmentFactory.py
+++ Assignments/Assignment4/GarmentFactory.py
@@ -54,9 +54,9 @@ class ShirtMenNika(ShirtMen):
     ShirtMenNika is a Nika Men's shirts that a garment factory makes.
     """
 
-    def __init__(self, style, size, colour, textile, sport_type):
+    def __init__(self, style, size, colour, textile, in_or_out):
         super().__init__(style, size, colour, textile)
-        self.sport_type = sport_type
+        self.in_or_out = in_or_out
 
 
 class ShirtWomen(abc.ABC):
@@ -102,9 +102,9 @@ class ShirtWomenNika(ShirtWomen):
     ShirtWomenNika is a Nika Women's shirts that a garment factory makes.
     """
 
-    def __init__(self, style, size, colour, textile, sport_type):
+    def __init__(self, style, size, colour, textile, in_or_out):
         super().__init__(style, size, colour, textile)
-        self.sport_type = sport_type
+        self.in_or_out = in_or_out
 
 
 class SockPairUnisex(abc.ABC):
@@ -152,3 +152,123 @@ class SockPairUnisexNika(SockPairUnisex):
         super().__init__(style, size, colour, textile)
         self.articulated = articulated
         self.length = length
+
+
+class Order:
+    def __init__(self, date=None, number=None, brand=None, garment=None,
+                 count=None, style=None, size=None, colour=None, textile=None,
+                 sport_type=None, num_hidden_pockets=None,
+                 require_drycleaning=None, in_or_out=None,
+                 require_ironing=None, num_buttons=None, articulated=None,
+                 length=None, contain_silver=None, stripe=None):
+        self.date = date
+        self.number = number
+        self.brand = brand
+        self.garment = garment
+        self.count = count
+        self.style = style
+        self.size = size
+        self.colour = colour
+        self.textile = textile
+        self.sport_type = sport_type
+        self.num_hidden_pockets = num_hidden_pockets
+        self.require_drycleaning = require_drycleaning
+        self.in_or_out = in_or_out
+        self.require_ironing = require_ironing
+        self.num_buttons = num_buttons
+        self.articulated = articulated
+        self.length = length
+        self.contain_silver = contain_silver
+        self.stripe = stripe
+
+
+class BrandFactory(abc.ABC):
+    """
+    The base factory class. All brands expect this factory class to populate
+    the brand. The BrandFactory class defines an interface to create a
+    Product famiily consisting of Men's shirts, Women's shirts, and Socks.
+    These vary by Brand.
+    """
+
+    @abc.abstractmethod
+    def create_shirt_men(self, order: Order) -> ShirtMen:
+        pass
+
+    @abc.abstractmethod
+    def create_shirt_women(self, order: Order) -> ShirtWomen:
+        pass
+
+    @abc.abstractmethod
+    def create_socks_unisex(self, order: Order) -> SockPairUnisex:
+        pass
+
+
+class LululimeFactory(BrandFactory):
+    """
+    This factory class implements the BrandFactory Interface. It returns
+    a product family consisting of ShirtMen, ShirtWomen, and SockPairUnisex
+    for the brand LuluLime.
+    """
+
+    def create_shirt_men(self, order: Order) -> ShirtMen:
+        return ShirtMenLuluLime(order.style, order.size, order.colour,
+                                order.textile, order.sport_type,
+                                order.num_hidden_pockets)
+
+    def create_shirt_women(self, order: Order) -> ShirtWomen:
+        return ShirtWomenLuluLime(order.style, order.size, order.colour,
+                                  order.textile, order.sport_type,
+                                order.num_hidden_pockets)
+
+    def create_socks_unisex(self, order: Order) -> SockPairUnisex:
+        return SockPairUnisexLuluLime(order.style, order.size, order.colour,
+                                      order.textile, order.contain_silver,
+                                      order.stripe)
+
+
+class PineappleRepublicFactory(BrandFactory):
+    """
+    This factory class implements the BrandFactory Interface. It returns
+    a product family consisting of ShirtMen, ShirtWomen, and SockPairUnisex
+    for the brand Pineapple Republic.
+    """
+
+    def create_shirt_men(self, order: Order) -> ShirtMen:
+        return ShirtMenPineappleRepublic(order.style, order.size, order.colour,
+                                         order.textile, order.sport_type,
+                                         order.num_hidden_pockets)
+
+    def create_shirt_women(self, order: Order) -> ShirtWomen:
+        return ShirtWomenPineappleRepublic(order.style, order.size,
+                                           order.colour, order.textile,
+                                           order.require_ironing,
+                                           order.num_buttons)
+
+    def create_socks_unisex(self, order: Order) -> SockPairUnisex:
+        return SockPairUnisexPineappleRepublic(order.style, order.size,
+                                               order.colour, order.textile,
+                                               order.require_drycleaning)
+
+
+class NikaFactory(BrandFactory):
+    """
+    This factory class implements the BrandFactory Interface. It returns
+    a product family consisting of ShirtMen, ShirtWomen, and SockPairUnisex
+    for the brand Nika.
+    """
+
+    def create_shirt_men(self, order: Order) -> ShirtMen:
+        return ShirtMenNika(order.style, order.size, order.colour,
+                            order.textile, order.in_or_out)
+
+    def create_shirt_women(self, order: Order) -> ShirtWomen:
+        return ShirtWomenNika(order.style, order.size, order.colour,
+                              order.textile, order.in_or_out)
+
+    def create_socks_unisex(self, order: Order) -> SockPairUnisex:
+        return SockPairUnisexNika(order.style, order.size, order.colour,
+                                  order.textile, order.articulated,
+                                  order.length)
+
+
+
