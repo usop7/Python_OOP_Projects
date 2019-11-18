68812bb38f9e352a4ae5134573f65bd1128dcf48
Leeseul Kim <dewkim7@gmail.com>
2019-11-17 18:48:19 -0800

    Created all types of products

--- Assignments/Assignment4/GarmentFactory.py
+++ Assignments/Assignment4/GarmentFactory.py
@@ -71,6 +71,42 @@ class ShirtWomen(abc.ABC):
         self.textile = textile
 
 
+class ShirtWomenLuluLime(ShirtWomen):
+    """
+    ShirtWomenLuluLime is a LuluLime Women's shirts that a garment factory
+    makes.
+    """
+
+    def __init__(self, style, size, colour, textile, sport_type,
+                 num_hidden_pockets):
+        super().__init__(style, size, colour, textile)
+        self.sport_type = sport_type
+        self.num_hidden_pockets = num_hidden_pockets
+
+
+class ShirtWomenPineappleRepublic(ShirtWomen):
+    """
+    ShirtWomenPineappleRepublic is a Pineapple Republic Women's shirts that
+    a garment factory makes.
+    """
+
+    def __init__(self, style, size, colour, textile, require_ironing,
+                 num_buttons):
+        super().__init__(style, size, colour, textile)
+        self.require_ironing = require_ironing
+        self.num_buttons = num_buttons
+
+
+class ShirtWomenNika(ShirtWomen):
+    """
+    ShirtWomenNika is a Nika Women's shirts that a garment factory makes.
+    """
+
+    def __init__(self, style, size, colour, textile, sport_type):
+        super().__init__(style, size, colour, textile)
+        self.sport_type = sport_type
+
+
 class SockPairUnisex(abc.ABC):
     """
     This defines the interface for Socks product.
@@ -81,3 +117,38 @@ class SockPairUnisex(abc.ABC):
         self.size = size
         self.colour = colour
         self.textile = textile
+
+
+class SockPairUnisexLuluLime(SockPairUnisex):
+    """
+    SockPairUnisexLuluLime is a LuluLime's socks product that a garment factory
+    makes.
+    """
+
+    def __init__(self, style, size, colour, textile, contain_silver,
+                 stripe):
+        super().__init__(style, size, colour, textile)
+        self.contain_silver = contain_silver
+        self.stripe = stripe
+
+
+class SockPairUnisexPineappleRepublic(SockPairUnisex):
+    """
+    SockPairUnisexPineappleRepublic is a Pineapple Republic socks product that
+    a garment factory makes.
+    """
+
+    def __init__(self, style, size, colour, textile, require_drycleaning):
+        super().__init__(style, size, colour, textile)
+        self.require_drycleaning = require_drycleaning
+
+
+class SockPairUnisexNika(SockPairUnisex):
+    """
+    SockPairUnisexNika is a Nika socks product that a garment factory makes.
+    """
+
+    def __init__(self, style, size, colour, textile, articulated, length):
+        super().__init__(style, size, colour, textile)
+        self.articulated = articulated
+        self.length = length
