567c89ca0b85d72a056805425e9d8e61551697d6
Leeseul Kim <dewkim7@gmail.com>
2019-11-21 00:40:26 -0800

    added producer thread

--- Labs/Lab9/producer_consumer.py
+++ Labs/Lab9/producer_consumer.py
@@ -1,5 +1,7 @@
 """This module """
 
+import threading
+import time
 from city_processor import CityOverheadTimes
 from city_processor import CityDatabase
 from city_processor import ISSDataRequest
@@ -26,13 +28,31 @@ class CityOverheadTimeQueue:
         return len(self.data_queue)
 
 
+class ProducerThread(threading.Thread):
+
+    def __init__(self, cities: list, queue: CityOverheadTimeQueue):
+        super().__init__()
+        self.cities = cities
+        self.queue = queue
+
+    def run(self) -> None:
+        dr = ISSDataRequest()
+        count = 0
+        for city in self.cities:
+            count += 1
+            self.queue.put(dr.get_overhead_pass(city))
+            if count % 5 == 0:
+                time.sleep(1)
+
+
 def main():
     q = CityOverheadTimeQueue()
     db = CityDatabase('city_locations.xlsx')
-    dr = ISSDataRequest()
-    for city in db.city_db:
-        q.put(dr.get_overhead_pass(city))
-    print(q.get())
+    queue = CityOverheadTimeQueue()
+    producer_thread = ProducerThread(db.city_db, queue)
+    producer_thread.start()
+    producer_thread.join()
+    print(*queue.data_queue, sep='')
 
 
 if __name__ == '__main__':
