8a4a2067f468d67a2d584a227b0bc13a29030d40
Leeseul Kim <dewkim7@gmail.com>
2019-11-25 10:32:50 -0800

    unittest: docstring, used assertIsInstance methods

--- Assignments/Assignment4/test_garment_factory.py
+++ Assignments/Assignment4/test_garment_factory.py
@@ -1,14 +1,8 @@
 from unittest import TestCase
-from garment_factory import GarmentMaker
-from garment_factory import OrderProcessor
-from garment_factory import Order
-from garment_factory import LululimeFactory
-from garment_factory import ShirtMenLuluLime
-from garment_factory import ShirtWomenPineappleRepublic
-from garment_factory import ShirtMen
-from garment_factory import ShirtWomen
-from garment_factory import SockPairUnisex
-from garment_factory import SockPairUnisexNika
+from garment_factory import GarmentMaker, OrderProcessor, Order, \
+    LululimeFactory, PineappleRepublicFactory, NikaFactory, ShirtMenLuluLime, \
+    ShirtWomenPineappleRepublic, ShirtMen, ShirtWomen, SockPairUnisex, \
+    SockPairUnisexNika
 
 
 class TestOrderProcessor(TestCase):
@@ -49,41 +43,50 @@ class TestOrderProcessor(TestCase):
                      count="3", style="Bowen", size="M", colour="Grey",
                      textile="Merino wool", articulated="Y", length="Knee")
 
-    @staticmethod
-    def create_garment_maker():
-        """
-        Creates a dictionary object, and loads the file into it.
-        :return: Dictionary
-        """
-        garment_maker = GarmentMaker()
-        order = TestOrderProcessor.create_lululime_shirtmen_order()
-        garment_maker.order_processor.order_list[0] = order
-        return garment_maker
-
-    def test_get_factory(self):
+    def test_get_lululime_factory(self):
         """
         Unit test for get_factory. Asserts that the method returns
-        LululimeFacotry when the order's brandtype is Lululime.
+        LululimeFacotry when the order's brand is Lululime.
         """
         order_processor = OrderProcessor()
         order = TestOrderProcessor.create_lululime_shirtmen_order()
         factory = order_processor.get_factory(order)
         self.assertTrue(isinstance(factory, LululimeFactory))
 
+    def test_get_pineapple_factory(self):
+        """
+        Unit test for get_factory method. Asserts that the method returns
+        PineappleRepublicFactory when the order's brand is PineappleRepublic.
+        """
+        order_processor = OrderProcessor()
+        order = TestOrderProcessor.create_pineapple_shirtwomen_order()
+        factory = order_processor.get_factory(order)
+        self.assertTrue(isinstance(factory, PineappleRepublicFactory))
+
+    def test_get_nika_factory(self):
+        """
+        Unit test for get_factory method. Asserts that the method returns
+        NikaFactory when the order's brand is Nika.
+        """
+        order_processor = OrderProcessor()
+        order = TestOrderProcessor.create_nika_socks_order()
+        factory = order_processor.get_factory(order)
+        self.assertTrue(isinstance(factory, NikaFactory))
+
     def test_create_shirt_men(self):
         """
-        Unit test for create_shirt_men. Asserts that the method creates
+        Unit test for create_shirt_men method. Asserts that the method creates
         ShirtMen based on its order.
         """
         order_processor = OrderProcessor()
         order = TestOrderProcessor.create_lululime_shirtmen_order()
         factory = order_processor.get_factory(order)
         garment = factory.create_shirt_men(order)
-        self.assertTrue(isinstance(garment, ShirtMen))
+        self.assertIsInstance(garment, ShirtMen)
 
     def test_shirt_men_maker(self):
         """
-        Unit test for shirt_men_maker. Asserts that the method creates
+        Unit test for shirt_men_maker method. Asserts that the method creates
         ShirtMenLuluLime and stores it in the garment_list dictionary.
         """
         maker = GarmentMaker()
@@ -91,22 +94,22 @@ class TestOrderProcessor(TestCase):
         maker.order_processor.order_list[1] = order
         maker.shirt_men_maker(order)
         garment = maker.garment_list.get(1)[0]
-        self.assertTrue(isinstance(garment, ShirtMenLuluLime))
+        self.assertIsInstance(garment, ShirtMenLuluLime)
 
     def test_create_shirt_women(self):
         """
-        Unit test for create_shirt_men. Asserts that the method creates
-        ShirtWomen based on its order.
+        Unit test for create_shirt_women method. Asserts that the method
+        creates ShirtWomen based on its order.
         """
         order_processor = OrderProcessor()
         order = TestOrderProcessor.create_pineapple_shirtwomen_order()
         factory = order_processor.get_factory(order)
         garment = factory.create_shirt_women(order)
-        self.assertTrue(isinstance(garment, ShirtWomen))
+        self.assertIsInstance(garment, ShirtWomen)
 
     def test_shirt_women_maker(self):
         """
-        Unit test for shirt_women_maker. Asserts that the method creates
+        Unit test for shirt_women_maker  method. Asserts that the method creates
         ShirtWomenPineappleRepublic and stores it in the garment_list dictionary.
         """
         maker = GarmentMaker()
@@ -114,22 +117,22 @@ class TestOrderProcessor(TestCase):
         maker.order_processor.order_list[1] = order
         maker.shirt_women_maker(order)
         garment = maker.garment_list.get(1)[0]
-        self.assertTrue(isinstance(garment, ShirtWomenPineappleRepublic))
+        self.assertIsInstance(garment, ShirtWomenPineappleRepublic)
 
     def test_create_socks_unisex(self):
         """
-        Unit test for create_socks_unisex. Asserts that the method creates
-        SockPairUnisex based on its order.
+        Unit test for create_socks_unisex method. Asserts that the method
+        creates SockPairUnisex based on its order.
         """
         order_processor = OrderProcessor()
         order = TestOrderProcessor.create_nika_socks_order()
         factory = order_processor.get_factory(order)
         garment = factory.create_socks_unisex(order)
-        self.assertTrue(isinstance(garment, SockPairUnisex))
+        self.assertIsInstance(garment, SockPairUnisex)
 
     def test_socks_unisex_maker(self):
         """
-        Unit test for socks_unisex_maker. Asserts that the method creates
+        Unit test for socks_unisex_maker method. Asserts that the method creates
         SockPairUnisexNika and stores it in the garment_list dictionary.
         """
         maker = GarmentMaker()
@@ -137,4 +140,4 @@ class TestOrderProcessor(TestCase):
         maker.order_processor.order_list[1] = order
         maker.socks_unisex_maker(order)
         garment = maker.garment_list.get(1)[0]
-        self.assertTrue(isinstance(garment, SockPairUnisexNika))
+        self.assertIsInstance(garment, SockPairUnisexNika)
